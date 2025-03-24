bbf7997fe8bb6ceb19ff8895dbfc7f88a6991e95
ID274 <id274@student.aru.ac.uk>
2025-03-24 13:22:45 +0000

    refactored the scoretest to use a proper teardown method

--- Assets/Tests/Editor/ScoreTest.cs
+++ Assets/Tests/Editor/ScoreTest.cs
@@ -1,19 +1,16 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 public class ScoreTest
 {
-    GameObject testObject = new GameObject();
+    GameObject testObject;
     ScoreManager scoreManager;
     ScoreCounter scoreCounter;
 
     [SetUp]
     public void SetUp()
     {
+        testObject = new GameObject();
         scoreManager = testObject.AddComponent<ScoreManager>();
         scoreCounter = testObject.AddComponent<ScoreCounter>();
         scoreManager.AssignScoreCounter(scoreCounter);
@@ -22,18 +19,24 @@ public class ScoreTest
     [Test]
     public void ScoreResetTest()
     {
-        scoreManager.Score = 100;
-        scoreManager.ResetScore();
+        scoreManager.Score += 10;
         Debug.Log($"Score: {scoreManager.Score}");
-        Assert.AreEqual(scoreManager.Score, 0);
+        Assert.AreEqual(scoreManager.Score, 10);
     }
 
     [Test]
     public void ScoreIncrementTest()
     {
-        scoreManager.ResetScore();
         scoreManager.IncrementScore();
         Debug.Log($"Score: {scoreManager.Score}");
         Assert.AreEqual(scoreManager.Score, 1);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        testObject = null;
+        scoreManager = null;
+        scoreCounter = null;
     }
 }
