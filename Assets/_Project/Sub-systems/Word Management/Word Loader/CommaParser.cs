590000507ab5f813f4bb1c389c1150b65ccd976f
ID274 <id274@student.aru.ac.uk>
2025-04-11 11:59:40 +0100

    refactored part of word loader to use interfaces and parser classes for parsing the file

---
+++ Assets/_Project/Sub-systems/Word Management/Word Loader/CommaParser.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommaParser : IFileParser
+{
+    public string[] GetWordsFromFile(TextAsset textfile)
+    {
+        if (textfile == null || string.IsNullOrEmpty(textfile.text))
+        {
+            Debug.LogError($"TextAsset {textfile} is null or empty.");
+            return null;
+        }
+
+        string[] words = textfile.text.Split(new[] { ',' }, System.StringSplitOptions.RemoveEmptyEntries);
+
+        return words;
+    }
+}
